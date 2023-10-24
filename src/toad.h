62a00d50719b8d0257aafbd561e7fa73c2fa25b0
Steve987321 <Stevebudding123@gmail.com>
2023-10-24 13:19:19 +0200

    Added a better sleep function for windows
    https://blat-blatnik.github.io/computerBear/making-accurate-sleep-function/

--- src/toad.h
+++ src/toad.h
@@ -213,4 +213,34 @@ namespace toad
         std::uniform_int_distribution<int> distribution(min, max);
         return distribution(gen);
     }
+
+    inline void precise_sleep(double seconds) {
+        using namespace std;
+        using namespace std::chrono;
+
+        static double estimate = 5e-3;
+        static double mean = 5e-3;
+        static double m2 = 0;
+        static int64_t count = 1;
+
+        while (seconds > estimate) {
+            auto start = high_resolution_clock::now();
+            this_thread::sleep_for(milliseconds(1));
+            auto end = high_resolution_clock::now();
+
+            double observed = (end - start).count() / 1e9;
+            seconds -= observed;
+
+            ++count;
+            double delta = observed - mean;
+            mean += delta / count;
+            m2 += delta * (observed - mean);
+            double stddev = sqrt(m2 / (count - 1));
+            estimate = mean + stddev;
+        }
+
+        // spin lock
+        auto start = high_resolution_clock::now();
+        while ((high_resolution_clock::now() - start).count() / 1e9 < seconds);
+    }
 }
